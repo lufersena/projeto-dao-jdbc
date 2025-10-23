23f2505a8346fb5bca96a308f09393b97086b5f3
Lucas Sena <lucasferreiracyrax@gmail.com>
2025-10-23 17:31:56 -0300

    findById and findAll implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("====TEST 1: department findById =====");
+		Department department = departmentDao.findById(3);
+		System.out.println(department);
+		
+		System.out.println("\n====TEST 2: department findAll =====");
+		List<Department> list = new ArrayList<>();
+		list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+	}
+
+}
